fbcb4d8a5d894bc0c0ef28762c9596af6f3df726
ImaneChafi <imane.chafi@mail.mcgill.ca>
2020-01-31 15:00:33 -0500

    Sessions work!! Added authentification on login page and redirect to / page!!

--- mycampus_backend/urls.py
+++ mycampus_backend/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', include('users.urls')),
     path('dashboard/', include('django.contrib.auth.urls')), # new
-
+    path('', TemplateView.as_view(template_name='home.html'), name='home'), # new
     ]
 
