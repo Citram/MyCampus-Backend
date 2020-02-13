04fafd2e52126e9980401985dfa966eb60dcbc00
cyrilyared <cyril@yared.info>
2020-02-13 16:39:43 -0500

    Fix DateTimeFIeld in event

--- events/models.py
+++ events/models.py
@@ -21,7 +21,7 @@ class Event(models.Model):
 
     name = models.CharField(max_length=100)
 
-    date = models.DateTimeField(default=datetime.date.today())
+    date = models.DateTimeField()
 
     fee = models.DecimalField(default=0, decimal_places=3, max_digits = 1000)
  
