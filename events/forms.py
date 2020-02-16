bdf78939add3f737cf5ceea330d9390e1c5f3ff2
Alex Choi <alexchoi713@gmail.com>
2020-02-16 09:29:39 -0500

    all event form tests are done, fixed minor bugs in view, forms, and made migrations once to make the tests run

--- events/forms.py
+++ events/forms.py
@@ -1,29 +1,132 @@
-from django.forms import ModelForm, Form
-from events.models import Event, Address
-from django.forms.fields import CharField
-#from pygeocoder import Geocoder
+import re
+import datetime
+from django import forms
+from .models import Event, Address
+from django.utils.translation import gettext_lazy as _
 
-class EventForm(ModelForm):
+
+class EventForm(forms.ModelForm):
+    """
+        not sure about this one, but for documentation on ModelForm, see
+        https://docs.djangoproject.com/en/3.0/topics/forms/modelforms/
+    """
     class Meta:
         model = Event
-        fields = ['name', 'date', 'fee', 'max_capacity', 'min_capacity', 'description', 'category']
+        fields = [
+            'name',
+            'date',
+            'fee',
+            'max_capacity',
+            'min_capacity',
+            'description',
+            'category'
+        ]
+        
 
-class AddressForm(ModelForm):
+    def clean_name(self):
+        name_input = self.cleaned_data['name']
+        if name_input is None or name_input.strip() == '':
+            raise forms.ValidationError(_('You cannot enter an empty name.'))
+        else:
+            return name_input
+
+    def clean_date(self):
+        date_input = self.cleaned_data['date']
+        if date_input.date() < datetime.date.today():
+            raise forms.ValidationError(_('You cannot enter an event date that is before today.'))
+        else:
+            return date_input
+
+    def clean_fee(self):
+        fee_input = self.cleaned_data['fee']
+        if fee_input < 0 or fee_input > 500 or None:
+            raise forms.ValidationError(_('You cannot enter a fee that is negative and you cannot try to scam people.'))
+        else:
+            return fee_input
+
+    def clean_max_capacity(self):
+        max_capacity_input = self.cleaned_data['max_capacity']
+        if max_capacity_input < 1 or max_capacity_input > 9000 or None:
+            raise forms.ValidationError(_('You cannot enter a maximum capacity less than 1 or more than 9000.'))
+        else:
+            return max_capacity_input
+
+    def clean_min_capacity(self):
+        min_capacity_input = self.cleaned_data['min_capacity']
+        if min_capacity_input < 1 or min_capacity_input > 9000 or None:
+            raise forms.ValidationError(_('You cannot enter a minimum capacity less than 1 or more than 9000.'))
+        # elif min_capacity_input > self.cleaned_data['max_capacity']:
+        #     raise forms.ValidationError(_('You cannot enter a minimum capacity higher than your maximum capacity.'))
+        else:
+            return min_capacity_input
+            
+    
+    def clean_description(self):
+        description_input = self.cleaned_data['description']
+        if description_input is None or description_input.strip() == '':
+            raise forms.ValidationError(_('You cannot enter an empty description.'))
+        else:
+            return description_input
+
+    def clean_category(self):
+        category_input = self.cleaned_data['category']
+        for option in Event.CATEGORIES:
+            if category_input == option[0]:
+                return category_input
+        raise forms.ValidationError(_('You cannot enter an invalid event category.'))
+
+
+class AddressForm(forms.ModelForm):
     class Meta:
         model = Address
-        fields = ['number', 'city', 'street', 'postalcode']
-    
-    def clean(self):
-        string_address = self.cleaned_data['number'] + " " + self.cleaned_data['street'] + ", " + self.cleaned_data['city'] + " " + self.cleaned_data['postalcode']
-        # g = Geocoder()
+        fields = [
+            'city',
+            'street',
+            'number',
+            'postalcode'
+        ]
 
-        # #if not g.geocode(string_address).valid_address:
-        #     #raise forms.ValidationError("Address is not valid.")
-        #     pass
-        return self.cleaned_data
+    def clean_city(self):
+        city_input = self.cleaned_data['city']
+        if city_input is None or city_input.strip == '':
+            raise forms.ValidationError(_('You cannot enter an empty city.'))
+        else:
+            return city_input
 
-class DeleteEventForm(Form):
-    id_field = CharField()
+    def clean_street(self):
+        street_input = self.cleaned_data['street']
+        if street_input is None or street_input.strip == '':
+            raise forms.ValidationError(_('You cannot enter an empty street.'))
+        else:
+            return street_input
 
+    def clean_number(self):
+        number_input = self.cleaned_data['number']
+        if number_input is None or number_input.strip == '':
+            raise forms.ValidationError(_('You cannot enter an empty street number.'))
+        else:
+            return number_input
 
+    def clean_postalcode(self):
+        postalcode_input = self.cleaned_data['postalcode']
+        if postalcode_input is None or postalcode_input.strip == '':
+            raise forms.ValidationError(_('You cannot enter an empty postal code,'))
+        else:
+            return postalcode_input
 
+class CommentForm(forms.Form):
+    #massage
+    message = forms.CharField(widget=forms.Textarea(),label='Your message: ')
+
+    event_id = forms.CharField(widget=forms.HiddenInput())
+
+    def clean_message(self):
+        message_input = self.cleaned_data['message']
+        if message_input is None or message_input.strip()=='':
+            raise forms.ValidationError('You cannot enter an empty message.')
+
+class DeleteEventForm(forms.Form):
+    id_field = forms.CharField()
+
+class RegistrationForm(forms.Form):
+    event_id = forms.CharField(widget=forms.HiddenInput())
---
+++ events/services.py
@@ -0,0 +1,134 @@
+from .models import *
+from users.models import *
+import datetime
+from django.http import JsonResponse
+from django.db.models import Q
+
+#====== Exception class ======#
+class UnsuccessfulOperationError(Exception):
+    def __init__(self, message, error_source):
+        super(UnsuccessfulOperationError, self).__init__(message)
+        self.error_source = error_source
+
+#================= Helper functions =================#
+def parse_datetime(time_string):
+    """
+    Parses the datetime string, from the ***toUTCString()*** method using javascript, into a datetime object
+    Example, takes "Tue, 22 Nov 2011 06:00:00 GMT" into the corresponding object
+    See this for details: https://stackoverflow.com/questions/8153631/js-date-object-to-python-datetime
+    """
+    datetimeobj = datetime.datetime.strptime(time_string, "%a, %d %b %Y %H:%M:%S %Z")
+    return datetimeobj
+
+def events_to_json(events):
+    """
+    renders a list of events to a dictionary to be passed into a JsonResponse
+    dictionary (list of events) contains a list of dictionaries (events)
+    each event contains id, name, date, fee, capacities
+    """
+    result = {}
+    index = 0
+    for e in events:
+        event = {}
+        event['id'] = e.id
+        event['name'] = e.name
+        event['date'] = e.date
+        event['fee'] = e.fee
+        event['max_capacity'] = e.max_capacity
+        event['min_capacity'] = e.min_capacity
+        result['event'+str(index)] = event
+        index += 1
+    return result
+
+#================= Services =================#
+
+def create_event(name_input, datetime_input, fee_input, min_capacity_input, max_capacity_input, description_input, category_input):
+    event = Event(
+        name=name_input,date=parse_datetime(datetime_input),
+        fee=fee_input,min_capacity=min_capacity_input, 
+        max_capacity=max_capacity_input, description=description_input,
+        category=category_input
+    )
+    event.save()
+
+
+def get_events_by_category(category_input):
+    events = Event.objects.filter(category=category_input)
+    if len(events) == 0:
+        raise UnsuccessfulOperationError('No events in this category has been found', 'no events found')
+    else:
+        result = JsonResponse(events_to_json(events))
+        return result
+
+def get_events_by_keywords(keywords):
+    # TODO instead of AND, how to we do OR???
+    # check this out but IDK how to deal with a for loop
+    # https://docs.djangoproject.com/en/3.0/topics/db/queries/#complex-lookups-with-q
+    events = Event.objects.all()
+    for kw in keywords:
+        events = events.filter(description__icontains=kw)
+    if len(events) == 0:
+        raise UnsuccessfulOperationError('No events with such description has been found', 'no events found')
+    else:
+        result = JsonResponse(events_to_json(events))
+        return result
+
+def get_events_by_words_in_name(words):
+    events = Event.objects.all()
+    for kw in words:
+        events = events.filter(name__icontains=kw)
+    if len(events) == 0:
+        raise UnsuccessfulOperationError('No events wiuth such name has been found', 'no events found')
+    else:
+        result = JsonResponse(events_to_json(events))
+        return result
+
+def delete_event(event_id):
+    try:
+        Event.objects.get(id=event_id).delete()
+    except:
+        raise UnsuccessfulOperationError('Event not found with id', 'event_id')
+
+def create_comment(event_id, user_id, message_input):
+    try:
+        event = Event.objects.get(id=event_id)
+    except:
+        raise UnsuccessfulOperationError('Event not found with id', 'event_id')
+    try:
+        user = RegularUser.objects.get(user_id)
+    except:
+        raise UnsuccessfulOperationError('User not found with id', 'user_id')
+
+    comment = Comment(message=message_input)
+    comment.author = user
+    comment.event = event
+    comment.save()
+
+def set_comment(comment_id, massage_input):
+    try:
+        comment = Comment.objects.get(id=comment_id)
+    except:
+        raise UnsuccessfulOperationError('Comment not found with id', 'comment_id')
+    comment.message = massage_input
+    comment.save()
+
+def delete_comment(comment_id):
+    try:
+        comment = Comment.objects.get(id=comment_id)
+    except:
+        raise UnsuccessfulOperationError('Comment not found with id', 'comment_id')
+    comment.delere()
+
+def join_event (user_id, event_id):
+    try:
+        event = Event.objects.get(id=event_id)
+    except:
+        raise UnsuccessfulOperationError('Event not found with id', 'event_id')
+    try:
+        user = Student.objects.get(user_id)
+    except:
+        raise UnsuccessfulOperationError('User not found with id', 'user_id')
+
+    event.attendees.add(user)
+    event.save()
+    return True
