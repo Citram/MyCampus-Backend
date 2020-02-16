ab4daf2d2a9d82395340d6169f04ff8e1eac65cb
konzert <konzert88@hotmail.com>
2020-02-16 16:26:26 -0500

    edited urls.py for querying. Not mergeable, still editing.

--- events/urls.py
+++ events/urls.py
@@ -3,10 +3,21 @@ from django.urls import path
 
 from . import views
 
+category = {
+    "category1": "entertainment",
+    "category2": "networking",
+    "category3": "educational",
+    "category4": "study",
+}
 
 urlpatterns = [
-    path('events/create/', views.create_event, name='create_event'),
-    path('events/delete/', views.delete_event, name='delete_event'),
-    path('', views.get_all_events, name='getallevents'),
-
+    path("events/create/", views.create_event, name="create_event"),
+    path("events/delete/", views.delete_event, name="delete_event"),
+    path("", views.get_all_events, name="getallevents"),
+    path(
+        "events/category/",
+        views.query_event_by_category(request=category),
+        name="query_event_list",
+    ),
 ]
+
