245f13e741feb999fb8c4d85fe1c249e6e4e22cd
ImaneChafi <imane.chafi@mail.mcgill.ca>
2020-02-17 12:05:45 -0500

    Added templates

--- events/urls.py
+++ events/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('events/create/', views.create_event, name='create_event'),
     path('events/delete/', views.delete_event, name='delete_event'),
     path('', views.get_all_events, name='getallevents'),
+    path('events/edit/', views.edit_event, name='edit_event'),
+
 
 ]
