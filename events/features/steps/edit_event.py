ccdd3dd49c4cf5c95b60dcd825619ecfe458cb7f
Alex Lo <hongtzang@gmail.com>
2020-02-23 12:16:37 -0500

    feature files for Query_Event_List, Edit_Event and Leave_Event created
    +ID003_Query_Event_List.feature
    +ID005_Edit_Event.feature
    +ID016_Leave_Event.feature
    and .py

---
+++ events/features/steps/edit_event.py
@@ -0,0 +1,28 @@
+from behave import *
+
+g_exception = None
+
+@when(u'the student requests to edit event JavaWorkshop')
+def step_impl(context):
+    pass
+    #raise NotImplementedError(u'STEP: When the student requests to edit event JavaWorkshop')
+
+
+@then(u'the event with name JavaWorkshop should be updated with its new fields and stored in the system')
+def step_impl(context):
+    pass
+    #raise NotImplementedError(u'STEP: Then the event with name JavaWorkshop should be updated with its new fields and stored in the system')
+
+
+@when(u'the student requests to edit an existing event without specifying at least one field')
+def step_impl(context):
+    pass
+    #raise NotImplementedError(u'STEP: When the student requests to edit an existing event without specifying at least one field')
+
+
+@when(u'the student requests to edit an event with at least one invalid field')
+def step_impl(context):
+    pass
+    #raise NotImplementedError(u'STEP: When the student requests to edit an event with at least one invalid field')
+
+
