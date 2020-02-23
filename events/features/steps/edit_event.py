fbecf8468576898c78066704376190348be95371
ImaneChafi <imane.chafi@mail.mcgill.ca>
2020-02-23 15:57:44 -0500

    Assert works

--- events/features/steps/edit_event.py
+++ events/features/steps/edit_event.py
@@ -16,14 +16,13 @@ def step_impl(step, id_element, name):
         editFinal_event(id_element, name,  event.datetime,  event.fee, event.description)
     except Exception as e:
         g_exception = e
-    #raise NotImplementedError(u'STEP: When the student requests to edit event JavaWorkshop')
 
 
 @then(u'the event with name {name} should be updated with its new fields and stored in the system')
 def step_impl(step, name):
     events = Event.objects.all()
-    pass
-    #raise NotImplementedError(u'STEP: Then the event with name JavaWorkshop should be updated with its new fields and stored in the system')
+    for event in events:
+        assert event.name == name
 
 
 @when(u'the student requests to edit an existing {id_element} without specifying at least one field an {error} is shown' )
@@ -34,8 +33,6 @@ def step_impl(context, id_element, error):
         editFinal_event(id_element, null, event.datetime,  event.fee, event.description)
     except Exception as e:
         pass
-
-    #raise NotImplementedError(u'STEP: When the student requests to edit an existing event without specifying at least one field')
 
 @when(u'the student requests to edit an {id_element} with at least one {invalid} field')
 def step_impl(context, id_element, invalid):
