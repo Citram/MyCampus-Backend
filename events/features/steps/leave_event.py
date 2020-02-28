a779ff90b7e1798d7c08ea65621e2039f722b53d
ImaneChafi <imane.chafi@mail.mcgill.ca>
2020-02-28 12:21:28 -0500

    Edited leave event acceptance test - works

--- events/features/steps/leave_event.py
+++ events/features/steps/leave_event.py
@@ -4,9 +4,14 @@ from events.models import Event, Address
 from create_new_event import create_events, save_events
 from events.services import delete_event_by_name
 
-@when(u'the student requests to leave event "Java Workshop"')
-def step_impl(context):
-    pass
+@when(u'the student requests to leave event "{id_element}"')
+def step_impl(step, id_element):
+    global g_exception
+    try:
+        event = Event.objects.get(id=id_element)
+        leave_event(id_element, event.id)
+    except Exception as e:
+        g_exception = e
     #raise NotImplementedError(u'STEP: When the student requests to leave event "Java Workshop"')
 
 
