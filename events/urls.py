2660a562f7fb101ab2e348a585e04aee3aabb312
Sofia Dieguez <sofia.dieguez@mail.mcgill.ca>
2020-03-02 14:44:49 -0500

    Update urls.py

--- events/urls.py
+++ events/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path('events/create/', views.create_event, name='create_event'),
     path('events/delete/', views.delete_event, name='delete_event'),
-    path('events/edit/', views.edit_event, name='editFinal_event'),
+    path('events/edit/', views.edit_event, name='edit_event'),
     path('', views.get_all_events, name='getallevents'),
 
 ]
